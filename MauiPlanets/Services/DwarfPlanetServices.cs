2c5714e91269a35e312da5216de3d1c4cbae4e28
KerttuRungi <kerttu.rungi@gmail.com>
2025-10-05 23:00:05 +0300

    Add more images to DwarfPlanetServices.cs

--- MauiPlanets/Services/DwarfPlanetServices.cs
+++ MauiPlanets/Services/DwarfPlanetServices.cs
@@ -21,7 +21,9 @@ namespace MauiPlanets.Services
                 AccentColorEnd = Color.FromArgb("#8d9098"),
                 Images = new()
                 {
-                  "https://science.nasa.gov/wp-content/uploads/2024/03/pluto-new-horizons-pia20291-16x9-1.jpg"
+                  "https://science.nasa.gov/wp-content/uploads/2024/03/pluto-new-horizons-pia20291-16x9-1.jpg",
+                  "https://assets.science.nasa.gov/dynamicimage/assets/science/psd/solar/internal_resources/5091/Pluto-1.jpeg?w=1280&h=900&fit=clip&crop=faces%2Cfocalpoint",
+                  "https://science.nasa.gov/wp-content/uploads/2024/10/poster-pluto-front-b-1.jpg?resize=582,900"
                 }
 
             },
@@ -35,7 +37,9 @@ namespace MauiPlanets.Services
                 AccentColorEnd = Color.FromArgb("#8d9098"),
                 Images = new()
                 {
-                  "https://assets.science.nasa.gov/dynamicimage/assets/science/astro/exo-explore/internal_resources/598/Ceres.jpeg?w=1041&h=1041&fit=clip&crop=faces%2Cfocalpoint"
+                  "https://assets.science.nasa.gov/dynamicimage/assets/science/astro/exo-explore/internal_resources/598/Ceres.jpeg?w=1041&h=1041&fit=clip&crop=faces%2Cfocalpoint",
+                  "https://science.nasa.gov/wp-content/uploads/2024/03/ceres-dawn-pia19562-16x9-1.jpg?resize=600,338",
+                  "https://science.nasa.gov/wp-content/uploads/2017/11/ceres-full-globe-pia21906-1920x640-1.jpg?resize=600,200"
                 }
             },
               new()
@@ -48,7 +52,9 @@ namespace MauiPlanets.Services
                 AccentColorEnd = Color.FromArgb("#8d9098"),
                 Images = new()
                 {
-                  "https://i0.wp.com/spacenews.com/wp-content/uploads/2022/10/Screen-Shot-2022-10-18-at-6.38.28-PM.png?w=864&ssl=1"
+                  "https://i0.wp.com/spacenews.com/wp-content/uploads/2022/10/Screen-Shot-2022-10-18-at-6.38.28-PM.png?w=864&ssl=1",
+                  "https://assets.science.nasa.gov/dynamicimage/assets/science/psd/solar/2023/09/h/haumea_th.jpg?w=320&h=240&fit=clip&crop=faces%2Cfocalpoint",
+                  "https://www.universetoday.com/article_images/haumea_nasa.jpg"
                 }
             },
 
@@ -62,22 +68,28 @@ namespace MauiPlanets.Services
                 AccentColorEnd = Color.FromArgb("#8d9098"),
                 Images = new()
                 {
-                  "https://cdn.mos.cms.futurecdn.net/8QBGiy9BhmttKdGeAAZpn8-430-80.jpg.webp"
+                  "https://cdn.mos.cms.futurecdn.net/8QBGiy9BhmttKdGeAAZpn8-430-80.jpg.webp",
+                  "https://assets.science.nasa.gov/dynamicimage/assets/science/psd/solar/internal_resources/5067/Makemake_moon-1.jpeg?w=800&h=600&fit=clip&crop=faces%2Cfocalpoint",
+                  "https://assets.science.nasa.gov/dynamicimage/assets/science/psd/solar/2023/09/m/makemake_th.jpg?w=320&h=240&fit=clip&crop=faces%2Cfocalpoint"
+
                 }
             },
-                new()
+
+            new()
+            {
+                Name = "Eris",
+                Subtitle = "The biggest dwarf planet",
+                HeroImage = "eris.png",
+                Description = "Eris is the second-largest known dwarf planet in the Solar System",
+                AccentColorStart = Color.FromArgb("#353535"),
+                AccentColorEnd = Color.FromArgb("#8d9098"),
+                Images = new()
                 {
-                    Name = "Eris",
-                    Subtitle = "The biggest dwarf planet",
-                    HeroImage = "eris.png",
-                    Description = "Eris is the second-largest known dwarf planet in the Solar System",
-                    AccentColorStart = Color.FromArgb("#353535"),
-                    AccentColorEnd = Color.FromArgb("#8d9098"),
-                    Images = new()
-                    {
-                    "https://solarsystem.nasa.gov/system/feature_items/images/144_eris_1600x900.jpg"
-                    }
+                 "https://www.solarsystemscope.com/spacepedia/images/handbook/renders/eris.png",
+                 "https://assets.science.nasa.gov/dynamicimage/assets/science/psd/solar/2023/09/e/ErisArtistConcept1200w.jpg?w=1200&h=900&fit=clip&crop=faces%2Cfocalpoint",
+                 "https://assets.science.nasa.gov/dynamicimage/assets/science/psd/solar/internal_resources/3256/Artists_c_oncept_of_Eris_and_Dysnomia_far_from_the_Sun.jpeg?w=800&h=600&fit=clip&crop=faces%2Cfocalpoint"
                 }
+            }
         };
 
         public static List<DwarfPlanet> GetAllDwarfPlanets()
